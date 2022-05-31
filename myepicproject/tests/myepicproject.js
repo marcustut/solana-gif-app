774bb1d48e970a57492af6e752745dc96e3db953
Marcus Lee <marcustutorial@hotmail.com>
2022-05-31 12:00:37 +0800

    added solana smart contract

---
+++ myepicproject/tests/myepicproject.js
@@ -0,0 +1,75 @@
+const anchor = require("@project-serum/anchor");
+
+// Need the system program
+const { SystemProgram } = anchor.web3;
+
+// describe("myepicproject", () => {
+//   // Configure the client to use the local cluster.
+//   anchor.setProvider(anchor.AnchorProvider.env());
+
+//   it("Is initialized!", async () => {
+//     // Add your test here.
+//     const program = anchor.workspace.Myepicproject;
+//     const tx = await program.methods.initialize().rpc();
+//     console.log("Your transaction signature", tx);
+//   });
+// });
+
+const main = async () => {
+  console.log("🚀 Starting test...");
+
+  // Create an set the provider. We set it before we needed to update it, so that it can communicate with our frontend!
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.Myepicproject;
+
+  // Create an account keypair for our program to use.
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  // Call initialize, pass it the params it needs!
+  let tx = await program.rpc.initialize({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
+
+  console.log("📝 Your transaction signature", tx);
+
+  // Fetch data from the account.
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("👀 GIF Count", account.totalGifs.toString());
+
+  // Call add_gif
+  await program.rpc.addGif(
+    "https://media.giphy.com/media/3o7aTnQqygA3TcukFi/giphy.gif",
+    {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+      },
+    }
+  );
+
+  // Get the account again to see what changed
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("👀 GIF Count", account.totalGifs.toString());
+
+  // Access gif_list on the account
+  console.log("👀 GIF List", account.gifList);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,32 @@ import { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import { connectWallet, getProvider, PhantomProvider } from "./lib/phantom";
 
+const TEST_GIFS = [
+  "https://i.giphy.com/media/eIG0HfouRQJQr1wBzz/giphy.webp",
+  "https://media3.giphy.com/media/L71a8LW2UrKwPaWNYM/giphy.gif?cid=ecf05e47rr9qizx2msjucl1xyvuu47d7kf25tqt2lvo024uo&rid=giphy.gif&ct=g",
+  "https://media4.giphy.com/media/AeFmQjHMtEySooOc8K/giphy.gif?cid=ecf05e47qdzhdma2y3ugn32lkgi972z9mpfzocjj6z1ro4ec&rid=giphy.gif&ct=g",
+  "https://i.giphy.com/media/PAqjdPkJLDsmBRSYUp/giphy.webp",
+];
+
 // Constants
-const TWITTER_HANDLE = "_buildspace";
+const TWITTER_HANDLE = "geminimarcus";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
   const [provider, setProvider] = useState<PhantomProvider>();
   const [walletAddress, setWalletAddress] = useState<string>();
+  const [inputValue, setInputValue] = useState<string>("");
+  const [gifList, setGifList] = useState<string[]>([]);
+
+  useEffect(() => setProvider(getProvider()), []);
 
   useEffect(() => {
-    setProvider(getProvider());
-  }, []);
+    if (walletAddress) {
+      console.log("Fetching GIF list...");
+
+      setGifList(TEST_GIFS);
+    }
+  }, [walletAddress]);
 
   const handleConnectWallet = useCallback(async () => {
     if (!provider) {
@@ -23,6 +38,16 @@ const App = () => {
     setWalletAddress(await connectWallet(provider));
   }, [provider]);
 
+  const sendGif = useCallback(async () => {
+    if (inputValue.length > 0) {
+      console.log("Gif link:", inputValue);
+      setGifList([...gifList, inputValue]);
+      setInputValue("");
+    } else {
+      console.log("Empty input. Try again.");
+    }
+  }, [inputValue]);
+
   const renderNotConnectedContainer = () => (
     <button
       className="cta-button connect-wallet-button"
@@ -30,6 +55,34 @@ const App = () => {
     >
       Connect to Wallet
     </button>
+  );
+
+  const renderConnectedContainer = () => (
+    <div className="connected-container">
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          sendGif();
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Enter gif link!"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button type="submit" className="cta-button submit-gif-button">
+          Submit
+        </button>
+      </form>
+      <div className="gif-grid">
+        {gifList.map((gif) => (
+          <div className="gif-item" key={gif}>
+            <img src={gif} alt={gif} />
+          </div>
+        ))}
+      </div>
+    </div>
   );
 
   return (
@@ -43,7 +96,9 @@ const App = () => {
                 View your GIF collection in the metaverse ✨
               </p>
             </div>
-            {!walletAddress && renderNotConnectedContainer()}
+            {!walletAddress
+              ? renderNotConnectedContainer()
+              : renderConnectedContainer()}
             <div className="footer-container">
               <img
                 alt="Twitter Logo"
